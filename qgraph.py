d7ef0d33d4d938a4ccb78dac30c42573e03b7495
qavan <pro100qavan@gmail.com>
2019-11-25 23:23:28 +0300

    skeleton of qgraph

--- qgraph.py
+++ qgraph.py
@@ -1,45 +1,54 @@
 class QGraph(object):
-    _name = 'QGraph class object'
+    _name = None
     _type = None
     _structure = []
 
     def __init__(self, name=None, typeof=None, structure=None):
         if typeof not in [None, 'AM', 'IM', 'AL', 'LE']:
-            raise Exception('A QGraph can only be represented as an adjacency matrix(AM), an incidence matrix(IM), an '
-                            'adjacency list(AL), and a list of edges(LE) or created manually(None)')
+            raise ValueError('A QGraph can only be represented as an adjacency matrix(AM), an incidence matrix(IM), an'
+                             ' adjacency list(AL), and a list of edges(LE) or created manually(None)')
         else:
+            self._name = name
             if typeof is not None:
-                self._name = name
                 self._type = typeof
-                if typeof is 'AL':
-                    self._structure = structure
-                elif typeof is 'AM':
-                    self._structure = structure
-                elif typeof is 'LE':
-                    self._structure = structure
-                elif typeof is 'IM':
+                if structure is not None:
                     self._structure = structure
                 else:
-                    raise Exception('A QGraph can only be represented as an adjacency matrix(AM), an incidence '
-                                    'matrix(IM), an adjacency list(AL), and a list of edges(LE) or created '
-                                    'manually(None)')
+                    raise ValueError('A QGraph can only be represented as an adjacency matrix(AM), an incidence '
+                                     'matrix(IM), an adjacency list(AL), and a list of edges(LE) or '
+                                     'created manually(None)')
             else:
-                self._name = name
+                self._type = None
 
     # def addNode(self, start, finis):
+    def getType(self):
+        return self._type
 
-    def __del__(self):
-        del self
+    def setName(self, name):
+        self._name = name
+        return self
 
     def getName(self):
         return self._name
 
-    def setName(self, name):
-        self._name = name
+    def delName(self):
+        del self._name
+        return self
+
+    def getStructure(self):
+        return self._structure
+
+    def updateStructure(self, structure):
+        self._structure = structure
+        return self
+
+    def clearStructure(self):
+        del self._structure
+        return self
 
     def draw(self):
         from vizualise import vizualise_0 as vis
         vis(self)
 
-qg = QGraph()
-qg.setName('Simple graph')
+    def __del__(self):
+        del self
