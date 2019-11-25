75e239b8810a69f5cc6b3bd430ced9a78a60b5ef
qavan <pro100qavan@gmail.com>
2019-11-25 23:23:01 +0300

    unittests init, added tests for skeleton

---
+++ tests.py
@@ -0,0 +1,60 @@
+import unittest
+from qgraph import QGraph
+
+
+class InitTests(unittest.TestCase):
+    def test_error_init_types(self):
+        """Test for representation of qgraphs with error situations"""
+        with self.assertRaises(ValueError):
+            QGraph(typeof='')
+        with self.assertRaises(ValueError):
+            QGraph(typeof='fuck')
+
+
+class TypeTests(unittest.TestCase):
+    def test_normal_get_type(self):
+        """Tests for representation settings of qgraphs w/out errors"""
+        self.assertEqual(QGraph(typeof='AL', structure={1: [2, 3], 2: [4]}).getType(), 'AL')
+        self.assertEqual(QGraph(typeof='AM', structure=[[0, 1], [1, 0]]).getType(), 'AM')
+        self.assertEqual(QGraph(typeof='LE', structure=[[1, 2], [1, 3], [2, 3]]).getType(), 'LE')
+        self.assertEqual(QGraph(typeof='IM', structure=[[1, 0, 1], [1, 1, 1], [0, 1, 0]]).getType(), 'IM')
+        self.assertIsNone(QGraph().getType())
+        self.assertIsNone(QGraph(typeof=None).getType())
+
+
+class NameTests(unittest.TestCase):
+    """Tests for getName"""
+    def test_name_get(self):
+        self.assertEqual(QGraph(name='TestName').getName(), 'TestName')
+    """Tests for setName > getName combos"""
+    def test_name_set_get(self):
+        self.assertEqual(QGraph().setName('TestName').getName(), 'TestName')
+    """Tests for setName > delName > getName combos"""
+    def test_name_set_del_get(self):
+        self.assertEqual(QGraph().setName('TestName').delName().getName(), None)
+
+
+class StructureTests(unittest.TestCase):
+    """Tests for getStructure"""
+    def test_structure_get(self):
+        self.assertEqual(QGraph().getStructure(), [])
+        self.assertEqual(QGraph(name='TestName').getStructure(), [])
+        self.assertEqual(QGraph(typeof='AL', structure={1: [2, 3], 2: [3]}).getStructure(), {1: [2, 3], 2: [3]})
+        self.assertEqual(QGraph(typeof='AM', structure=[[0, 0], [1, 0]]).getStructure(), [[0, 0], [1, 0]])
+        self.assertEqual(QGraph(typeof='LE', structure=[[1, 3], [1, 2]]).getStructure(), [[1, 3], [1, 2]])
+        self.assertEqual(QGraph(typeof='IM', structure=[[1, 1], [1, 1]]).getStructure(), [[1, 1], [1, 1]])
+    """Tests for updateStructure > getStructure combos"""
+    def test_structure_update_get(self):
+        self.assertEqual(QGraph().updateStructure([[0, 0], [1, 0]]).getStructure(), [[0, 0], [1, 0]])
+    """Tests for clearStructure > getStructure combos"""
+    def test_structure_clear_get(self):
+        self.assertEqual(QGraph().clearStructure().getStructure(), [])
+    """Tests for updateStructure > clearStructure > getStructure combos"""
+    def test_structure_update_clear_get(self):
+        self.assertEqual(QGraph().updateStructure([[0, 0], [1, 0]]).clearStructure().getStructure(), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
